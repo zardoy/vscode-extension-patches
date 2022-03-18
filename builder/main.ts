4498ed7c656dc54b23a91fd78789b696c4b968f4
Vitaly Turovsky <vital2580@icloud.com>
2022-03-18 04:00:16 +0300

    [skip ci] release with wrapper

--- builder/main.ts
+++ builder/main.ts
@@ -1,7 +1,7 @@
-/* eslint-disable unicorn/filename-case */
 /* eslint-disable no-await-in-loop */
 import { join } from 'path'
 import fs from 'fs'
+import { ManifestType } from 'vscode-manifest'
 import { SetRequired } from 'type-fest'
 import parseArgv from 'minimist'
 import { trueCasePath } from 'true-case-path'
@@ -10,20 +10,22 @@ import execa, { Options as ExecaOptions } from 'execa'
 import fsExtra from 'fs-extra'
 import { modifyPackageJsonFile } from 'modify-json-file'
 import gitly from 'gitly'
-import { readJsonFile } from 'typed-jsonfile'
+import { readJsonFile, readPackageJsonFile, writePackageJsonFile } from 'typed-jsonfile'
 import dedent from 'string-dedent'
 import globby from 'globby'
+import { build } from 'esbuild'
 import { parsePatchFile } from './patch-package/types/patch/parse'
 import { executeEffects } from './patch-package/types/patch/apply'
-import { build } from 'esbuild'
 
 // git diff --cached > mypatch.patch
 
 export interface AnyLevelMetadata {
-    packageJson?: Record<string, any>
     build: string
     precommand: string
     postfixDisplayName: string
+    packageJson?: Record<string, any>
+    skipPrepublish?: boolean
+    esbuildOptions?: Record<string, any>
 }
 
 export interface MetadataJson extends Partial<AnyLevelMetadata> {
@@ -62,7 +64,7 @@ const main = async () => {
     const basePath = join(__dirname, '..')
     const extensionsPath = join(basePath, 'extended-extensions/extensions')
     const extensionsList = await fs.promises.readdir(extensionsPath)
-    const globalLevelMetadata = await readJsonFile<GlobalMetadataJson>(join(__dirname, './metadata.jsonc'))
+    const globalLevelMetadata = await readJsonFile<GlobalMetadataJson>(join(extensionsPath, '../metadata.jsonc'))
     const argv = parseArgv<{
         // extensions only
         ext: string
@@ -83,7 +85,7 @@ const main = async () => {
         const fromCache = (...path: string[]) => join(basePath, 'source-cache', extension, ...path)
         const fromDest = (...path: string[]) => join(basePath, 'dest', extension, ...path)
         const fromDestExtension = (...path: string[]) => fromDest(localLevelMetadata.location ?? '', ...path)
-        const fromTemp = (...path: string[]) => fromDest(basePath, 'temp', extension, ...path)
+        const fromTemp = (...path: string[]) => join(basePath, 'temp', extension, ...path)
 
         console.log('Extension target:', fromDestExtension())
         if (!fs.existsSync(fromCache())) await gitly(localLevelMetadata.repo, fromCache(), {})
@@ -91,24 +93,25 @@ const main = async () => {
         await fsExtra.copy(fromCache(), fromDest())
         // TODO make pkg from release-action
 
-        let manifest
-        let originalFullId: string
+        const newEntrypoint = fromDestExtension('main.js')
+        if (fs.existsSync(newEntrypoint)) throw new Error(`Conflicting file in ext dir: ${newEntrypoint}`)
+
+        let manifest!: ManifestType
+        let originalManifest!: ManifestType
+        let originalFullId!: string
+        let mainScriptOverride: string | undefined
         await modifyPackageJsonFile({ dir: fromDestExtension() }, manifestUntyped => {
-            originalFullId = `${manifest.publisher}.${manifest.name}`
             manifest = manifestUntyped as any
-            manifest.originalDisplayName = manifest.displayName
-            if (!manifest.main) throw new Error('No main script')
-            manifest.originalMain = manifest.main
+            originalManifest = { ...manifest }
+            originalFullId = `${manifest.publisher}.${manifest.name}`
             assignDefined(manifest, {
-                // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
                 displayName: manifest.displayName + mergedMetadata.postfixDisplayName,
             })
             // TODO restore browser field
-            Object.assign(manifest, { /* main: './main.js', */ browser: undefined, icon: undefined, ...mergedMetadata.packageJson })
+            Object.assign(manifest, { main: './main.js', browser: undefined, icon: undefined, ...mergedMetadata.packageJson })
+            mainScriptOverride = mergedMetadata.packageJson?.main
             return manifest
         })
-        // TODO
-        // await fs.promises.writeFile(fromDestExtension('./main.js'), `module.exports = require('./${manifest.originalMain}')`)
         const originalReadmePath = await trueCasePath('readme.md', fromDestExtension())
         // const readmeContents = await fs.promises.readFile(originalReadmePath, 'utf-8')
         // ensure readme is always in lowercase
@@ -118,7 +121,7 @@ const main = async () => {
             fromDestExtension('README.MD'),
             dedent`
               # ${manifest.displayName}
-              ❗❗❗ This is extended version of [${manifest.originalDisplayName}](https://marketplace.visualstudio.com/items?itemName=${originalFullId}). You need to disable original extension in order this extension to work!
+              ❗❗❗ This is extended version of [${originalManifest.displayName}](https://marketplace.visualstudio.com/items?itemName=${originalFullId}). You need to disable original extension in order this extension to work!
               ${newReadmeContents}
             `,
         )
@@ -138,13 +141,63 @@ const main = async () => {
         // if (argv.dev)
         await execute(mergedMetadata.build, { cwd: fromDestExtension() })
 
+        const prepublishScriptContents = originalManifest.scripts?.['vscode:prepublish']
+        if (prepublishScriptContents) {
+            await modifyPackageJsonFile({ dir: fromDestExtension() }, pkg => {
+                pkg.scripts!['vscode:prepublish'] = undefined
+                return pkg
+            })
+            if (mergedMetadata.skipPrepublish !== false) {
+                await execute(prepublishScriptContents, { cwd: fromDestExtension() })
+                if (!mainScriptOverride) originalManifest.main = (await readPackageJsonFile({ dir: fromDestExtension() })).main
+            }
+        }
+
+        await fsExtra.emptyDir(fromTemp())
+
+        // const entrypoint = fromTemp('entrypoint.ts');
+        // await fsExtra.copyFile(join(__dirname, 'wrapper.ts'), entrypoint)
+
+        const originalEntrypoint = mainScriptOverride ?? originalManifest.main!
+
+        if (!originalEntrypoint) throw new Error('No main entry in package.json')
+        // if (!fs.existsSync(originalEntrypoint)) throw new Error("Entrypoint doesn't exist")
+
+        await build({
+            bundle: true,
+            // entryPoints: [entrypoint],
+            entryPoints: [join(__dirname, 'wrapper.ts')],
+            outfile: newEntrypoint,
+            platform: 'node',
+            target: 'node14',
+            // minify:true,
+            // metafile: true,
+            format: 'cjs',
+            external: [originalEntrypoint, 'vscode'],
+            define: {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                __ORIGINAL_EXTENSION_ID__: `"${originalFullId}"`,
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                __INJECT__ENTRYPOINT__: 'null',
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                __ORIGINAL_ENTRYPOINT__: `"${originalEntrypoint}"`,
+            },
+            ...mergedMetadata.esbuildOptions,
+        })
+
         if (argv.release) {
-            const vsixPath = fromDest('output.vsix')
+            const vsixPath = fromTemp('output.vsix')
             // TODO change path to more safe
             // skipping ovsx just for now
             await execa('vsce', ['package', '--out', vsixPath], { stdio: 'inherit', cwd: fromDestExtension() })
             if (process.env.CI) await execa('vsce', ['publish', '--packagePath', vsixPath], { stdio: 'inherit', cwd: fromDestExtension() })
         }
+
+        if (prepublishScriptContents)
+            await modifyPackageJsonFile({ dir: fromDestExtension() }, pkg => {
+                pkg.scripts!['vscode:prepublish'] = prepublishScriptContents
+                return pkg
+            })
     }
 }
 
--- builder/wrapper.ts
+++ builder/wrapper.ts
@@ -1,13 +1,20 @@
+/* eslint-disable unicorn/no-abusive-eslint-disable */
+/* eslint-disable */
 import * as vscode from 'vscode'
+
+declare const __ORIGINAL_EXTENSION_ID__: string
+declare const __INJECT__ENTRYPOINT__: string
+declare const __ORIGINAL_ENTRYPOINT__: string
 
 // actual code for every extension
 export const activate = context => {
-    const originalExtension = vscode.extensions.getExtension('ORIGINAL_EXTENSION_ID')
+    const originalExtension = vscode.extensions.getExtension(__ORIGINAL_EXTENSION_ID__)
     // TODO wether to check isActive
     if (originalExtension) {
         void vscode.window.showErrorMessage('Disable original version in order this extension to work.')
         return
     }
-    //INJECT_CODE
-    require('ORIGINAL_ENTRYPOINT')
+
+    __INJECT__ENTRYPOINT__ && require(__INJECT__ENTRYPOINT__)
+    require(__ORIGINAL_ENTRYPOINT__)
 }
